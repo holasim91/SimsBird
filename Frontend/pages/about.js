3d749cc4f5cf2e43630484030114744c11b30fee
HyunIn Sim <>
2020-08-14 13:51:48 +0900

    alive about.js

--- Frontend/pages/about.js
+++ Frontend/pages/about.js
@@ -1,59 +1,59 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import Head from 'next/head';
-// import { END } from 'redux-saga';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Head from 'next/head';
+import { END } from 'redux-saga';
 
-// import { Avatar, Card } from 'antd';
-// import AppLayout from '../components/AppLayout';
-// import wrapper from '../store/configureStore';
-// import { LOAD_USER_REQUEST } from '../reducers/user';
+import { Avatar, Card } from 'antd';
+import AppLayout from '../components/AppLayout';
+import wrapper from '../store/configureStore';
+import { LOAD_USER_REQUEST } from '../reducers/user';
 
-// const About = () => {
-//   const { userInfo } = useSelector((state) => state.user);
+const About = () => {
+  const { userInfo } = useSelector((state) => state.user);
 
-//   return (
-//     <AppLayout>
-//       <Head>
-//         <title>SBD | SimsBird</title>
-//       </Head>
-//       {userInfo ? (
-//         <Card
-//           actions={[
-//             <div key="twit">
-//               짹짹
-//               <br />
-//               {userInfo.Posts}
-//             </div>,
-//             <div key="following">
-//               팔로잉
-//               <br />
-//               {userInfo.Followings}
-//             </div>,
-//             <div key="follower">
-//               팔로워
-//               <br />
-//               {userInfo.Followers}
-//             </div>,
-//           ]}
-//         >
-//           <Card.Meta
-//             avatar={<Avatar>{userInfo.nickname[0]}</Avatar>}
-//             title={userInfo.nickname}
-//             description="WHO AM I?"
-//           />
-//         </Card>
-//       ) : null}
-//     </AppLayout>
-//   );
-// };
+  return (
+    <AppLayout>
+      <Head>
+        <title>SBD | SimsBird</title>
+      </Head>
+      {userInfo ? (
+        <Card
+          actions={[
+            <div key="twit">
+              짹짹
+              <br />
+              {userInfo.Posts}
+            </div>,
+            <div key="following">
+              팔로잉
+              <br />
+              {userInfo.Followings}
+            </div>,
+            <div key="follower">
+              팔로워
+              <br />
+              {userInfo.Followers}
+            </div>,
+          ]}
+        >
+          <Card.Meta
+            avatar={<Avatar>{userInfo.nickname[0]}</Avatar>}
+            title={userInfo.nickname}
+            description="WHO AM I?"
+          />
+        </Card>
+      ) : null}
+    </AppLayout>
+  );
+};
 
-// export const getStaticProps = wrapper.getStaticProps(async (context) => {
-//   console.log('getStaticProps');
-//   context.store.dispatch({
-//     type: LOAD_USER_REQUEST,
-//     data: 1,
-//   });
-//   context.store.dispatch(END);
-//   await context.store.sagaTask.toPromise();
-// }); //  절대 바뀌지 않는 화면일 때 사용한다
-// export default About;
+export const getStaticProps = wrapper.getStaticProps(async (context) => {
+  console.log('getStaticProps');
+  context.store.dispatch({
+    type: LOAD_USER_REQUEST,
+    data: 1,
+  });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+}); //  절대 바뀌지 않는 화면일 때 사용한다
+export default About;
